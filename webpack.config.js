8f88ef68c5aa03520a92586c45d5c4142c22fe16
John Cheesman <me@johncheesman.org.uk>
2018-02-27 06:40:11 +0000

    Upgrade to phaser@3.1.2 and webpack@4.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,46 @@
 const path = require('path')
 const webpack = require('webpack')
-const env = process.env.NODE_ENV
 
-let config = {
-    entry: './src/index.js',
-    devtool: 'source-map',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist'),
-        publicPath: "/"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel-loader'],
-                exclude: /node_modules/
-            }
+module.exports = env => {
+    console.log(env.production)
+
+    return {
+        entry: {
+            main: './src/index.js'
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.join(__dirname, 'dist'),
+            publicPath: "/"
+        },
+        mode: env.production ? 'production' : 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+                },
+                {
+                    test: [/\.vert$/, /\.frag$/],
+                    use: 'raw-loader'
+                }
+            ]
+        },
+        devServer: {
+            contentBase: './dist'
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'CANVAS_RENDERER': JSON.stringify(true),
+                'WEBGL_RENDERER': JSON.stringify(true)
+            })
         ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({  
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ]
+    }
 }
-
-if (env === 'production') {
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin())
-}
-
-module.exports = config
 
